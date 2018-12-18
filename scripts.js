aa934c4ebc090775f85af839f318386b2097a29a
Robert Ragans <robertragans@Roberts-MacBook-Pro-2.local>
2018-12-18 15:29:36 -0500

    added multiple entries

--- scripts.js
+++ scripts.js
@@ -2,18 +2,30 @@ $(document).ready(()=>{
     $('.stock-form').submit((event)=>{
         event.preventDefault()
         const symbol = $('#symbol').val()
-        const url = `https://api.iextrading.com/1.0/stock/${symbol}/quote`
-        $.getJSON(url,(dataFound)=>{
-            console.log(dataFound)
-            $('#stock-body').html(`
-            <tr>
-                <td>${dataFound.symbol}</td>
-                <td>${dataFound.companyName}</td>
-                <td>${dataFound.high}</td>
-                <td>${dataFound.low}</td>
-                <td>${dataFound.change}</td>
-            </tr>
-            `)
+        $('#symbol').val('')
+        const symbols =symbol.split(',')
+        symbols.forEach((s)=>{
+            s = s.trim()
+            const url = `https://api.iextrading.com/1.0/stock/${s}/quote`
+            $.getJSON(url,(dataFound)=>{
+                console.log(dataFound)
+                let changeClass =""
+                if (dataFound.change > 0){
+                    changeClass = "bg-success"
+                }else{
+                    changeClass = "bg-danger"
+                }
+                $('#stock-body').prepend(`
+                <tr>
+                    <td>${dataFound.symbol}</td>
+                    <td>${dataFound.companyName}</td>
+                    <td>${dataFound.high}</td>
+                    <td>${dataFound.low}</td>
+                    <td class=${changeClass}>${dataFound.change}</td>
+                </tr>
+                `)
+                $('#stock-table').DataTable()
+            })
         })
     })
 })
